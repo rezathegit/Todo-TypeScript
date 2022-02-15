b6f0ad5bd1a3796d1877964127948cebdc00447e
Salt Dev <asadimohamadreza@gmail.com>
2022-02-15 14:46:56 +0100

    single todo responsive

--- src/components/SingleTodo/SingleTodo.tsx
+++ src/components/SingleTodo/SingleTodo.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import "./SingleTodo.css"
+import React from 'react';
+import "./SingleTodo.css";
+import { Todo } from "../../model";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai"
+import { MdDone } from "react-icons/md"
 
+type Props = {
+    todo: Todo;
+    todos: Todo[];
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
 
-const SingleTodo = () => {
+const SingleTodo = ({ todo, todos, setTodos }:Props) => {
   return (
-    <div>SingleTodo</div>
+    <form className="todos__single">
+        <span className="todos__single--text">
+            {todo.todo}
+        </span>
+        <div>
+            <span className="icon">
+                <AiFillEdit/>
+            </span>
+            <span className="icon">
+            <AiFillDelete/>
+            </span>
+            <span className="icon">
+                <MdDone/>
+            </span>
+        </div>
+    </form>
   )
 }
 
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Todo } from '../../model';
+import SingleTodo from '../SingleTodo/SingleTodo';
 import "./TodoList.css";
 
 interface Props {
@@ -10,8 +11,14 @@ interface Props {
 const TodoList: React.FC<Props> = ({ todos, setTodos }:Props) => {
   return (
     <div className="todos">
+      
       {todos.map((todo) => (
-        <li>{todo.todo}</li>
+        <SingleTodo 
+        todo={todo} 
+        key={todo.id}
+        todos={todos}
+        setTodos={setTodos}
+         />
       ))}
       </div>
   )
